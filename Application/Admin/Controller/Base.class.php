5200d599d40bb6b04ee726f971f1459bf26a4552
hecong0607 <748968893@qq.com>
2016-08-18 23:33:49 +0800

    修复上一条

--- Application/Admin/Controller/Base.class.php
+++ Application/Admin/Controller/Base.class.php
@@ -14,14 +14,6 @@ use Think\Controller;
 class Base extends Controller
 {
 	function __construct() {
-		ECHO 1;
-		$user = new UserModel();
-		$where['id'] = ':id';
-		$bind[':id'] = array(1,\PDO::PARAM_INT);
-		$result = $user->where($where)->bind($bind)->find();
-		var_dump($result,$user->getLastSql());die;
-
-
 		parent::__construct();
 		$this->checkIsLogin();
 	}
